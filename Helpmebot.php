6271396576f8cb39f89f37a6b00aaa409c001dba
stwalkerster <github@stwalkerster.co.uk>
2021-11-14 04:10:28 +0000

    rm cmdautogen/command ns, plus brain specials

--- Helpmebot.php
+++ Helpmebot.php
@@ -7,12 +7,8 @@ if(!defined('MEDIAWIKI')) {
 	die( 1 );
 }
 
-define('NS_COMMAND', 204);
-define('NS_COMMAND_TALK', 205);
 define('NS_MESSAGE', 206);
 define('NS_MESSAGE_TALK', 207);
-define('NS_CMDAUTOGEN', 208);
-define('NS_CMDAUTOGEN_TALK', 209);
 
 $wgExtensionCredits['specialpage'][] = array(
 'path' => __FILE__,
@@ -22,26 +18,16 @@ $wgExtensionCredits['specialpage'][] = array(
 );
 
 $wgAvailableRights[] = 'helpmebot-editmessages';
-$wgAvailableRights[] = 'helpmebot-editautogen';
 
-$wgExtraNamespaces[NS_COMMAND] = "Command";
-$wgExtraNamespaces[NS_COMMAND_TALK] = "Command_talk:";
 $wgExtraNamespaces[NS_MESSAGE] = "Message";
 $wgExtraNamespaces[NS_MESSAGE_TALK] = "Message_talk:";
-$wgExtraNamespaces[NS_CMDAUTOGEN] = "CmdAutogen";
-$wgExtraNamespaces[NS_CMDAUTOGEN_TALK] = "CmdAutogen_talk:";
 
 $wgNamespaceProtection[NS_MESSAGE]      = array( 'helpmebot-editmessages' );
-$wgNamespaceProtection[NS_CMDAUTOGEN]      = array( 'helpmebot-editautogen' );
 
 $wgAutoloadClasses['SpecialAccessList'] = $IP . '/extensions/Helpmebot/SpecialAccessList.php';
 $wgAutoloadClasses['AccessListPager'] = $IP . '/extensions/Helpmebot/AccessListPager.php';
 
-$wgAutoloadClasses['SpecialBrain'] = $IP.'/extensions/Helpmebot/SpecialBrain.php';
-$wgAutoloadClasses['BrainPager'] = $IP.'/extensions/Helpmebot/BrainPager.php';
-
 $wgSpecialPages['AccessList'] = 'SpecialAccessList';
-$wgSpecialPages['Brain'] = 'SpecialBrain';
 
 $wgExtensionMessagesFiles['Helpmebot'] = $IP . '/extensions/Helpmebot/Helpmebot.i18n.php';
 
