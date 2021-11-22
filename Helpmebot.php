1cea070df120116fb215243b4116537f4e9923f3
stwalkerster <simon@stwalkerster.co.uk>
2021-11-22 13:44:56 +0000

    Final deprecation

--- Helpmebot.php
+++ Helpmebot.php
@@ -7,30 +7,9 @@ if(!defined('MEDIAWIKI')) {
 	die( 1 );
 }
 
-define('NS_MESSAGE', 206);
-define('NS_MESSAGE_TALK', 207);
-
 $wgExtensionCredits['specialpage'][] = array(
 'path' => __FILE__,
 'name' => 'Helpmebot Viewer',
-'description' => 'Allows viewing of Helpmebot\'s configuration etc.',
+'description' => 'Deprecated and no longer required for Helpmebot deployments',
 'author' => 'Simon Walker'
 );
-
-$wgAvailableRights[] = 'helpmebot-editmessages';
-
-$wgExtraNamespaces[NS_MESSAGE] = "Message";
-$wgExtraNamespaces[NS_MESSAGE_TALK] = "Message_talk:";
-
-$wgNamespaceProtection[NS_MESSAGE]      = array( 'helpmebot-editmessages' );
-
-$wgExtensionMessagesFiles['Helpmebot'] = $IP . '/extensions/Helpmebot/Helpmebot.i18n.php';
-
-$wgResourceModules['ext.Helpmebot'] = array(
-	'styles' => 'modules/hmb.css',
-	'position' => 'top',
-	'localBasePath' => __DIR__,
-	'remoteExtPath' => 'Helpmebot'
-);
-
-$wgHelpmebotStyleVersion=6;
